84e9947ba30bdad06e597f526a80a16f3f0b815b
Afraz Khan <afrazk21@gmail.com>
2024-07-27 14:07:39 -0600

    updated app.js to take in multiple queries

--- my-express-app/app.js
+++ my-express-app/app.js
@@ -58,7 +58,7 @@ const getRedditAuthToken = async () => {
             console.log('Successfully fetched search results');
 
             const posts = searchResponse.data.data.children.map((child) => {
-                const { title, selftext, author, ups, score, num_comments, created_utc, permalink, num_crossposts } = child.data;
+                const { title, selftext, author, ups, score, num_comments, created_utc, num_crossposts } = child.data;
                 res_final.push({
                     title: title,
                     description: selftext,
@@ -71,12 +71,13 @@ const getRedditAuthToken = async () => {
                 });
             });
         }
-        const filePath = path.join(__dirname, 'reddit_search_results.json');
+        // const filePath = path.join(__dirname, 'reddit_search_results.json');
 
-        // Write the results to a JSON file
-        fs.writeFileSync(filePath, JSON.stringify(res_final, null, 2));
+        // // Write the results to a JSON file
+        // fs.writeFileSync(filePath, JSON.stringify(res_final, null, 2));
 
-        console.log(`Results written to ${filePath}`);
+        // console.log(`Results written to ${filePath}`);
+        return res_final;
     } catch (error) {
         console.error('Error occurred during Reddit search:', error.message);
         throw error;
@@ -97,7 +98,23 @@ app.get('/search', async (req, res) => {
     const queries = ["jasper wildfire", "Edmonton wildfire", "California wildfire", "Edmonton Wildfire smoke", "Edmonton Wildfire evacuation"
     , "Edmonton Wildfire air quality", "Edmonton Wildfire news", "Edmonton Wildfire update", "Edmonton Wildfire evacuation order", "Edmonton Wildfire evacuation alert",
     "Edmonton Wildfire evacuation warning", "Edmonton Wildfire evacuation zone", "Edmonton Wildfire evacuation map", "Edmonton Wildfire evacuation route", "Edmonton Wildfire evacuation plan",
-    "Edmonton Wildfire evacuation checklist", "Edmonton Wildfire evacuation kit", "Edmonton Wildfire evacuation center", "Edmonton Wildfire evacuation shelter", "Edmonton Wildfire evacuation hotel"
+    "Edmonton Wildfire evacuation checklist", "Edmonton Wildfire evacuation kit", "Edmonton Wildfire evacuation center", "Edmonton Wildfire evacuation shelter", "Edmonton Wildfire evacuation hotel",
+    "Alberta Wildfire", "Alberta Wildfire smoke", "Alberta Wildfire evacuation", "Alberta Wildfire air quality", "Alberta Wildfire news", "Alberta Wildfire update", "Alberta Wildfire evacuation order",
+    "Jasper Wildfire", "Jasper Wildfire smoke", "Jasper Wildfire evacuation", "Jasper Wildfire air quality", "Jasper Wildfire news", "Jasper Wildfire update", "Jasper Wildfire evacuation order",
+    "Jasper Wildfire evacuation alert", "Jasper Wildfire evacuation warning", "Jasper Wildfire evacuation zone", "Jasper Wildfire evacuation map", "Jasper Wildfire evacuation route", "Jasper Wildfire evacuation plan",
+    "Jasper Wildfire evacuation checklist", "Jasper Wildfire evacuation kit", "Jasper Wildfire evacuation center", "Jasper Wildfire evacuation shelter", "Jasper Wildfire evacuation hotel",
+    "Red Deer Wildfire", "Red Deer Wildfire smoke", "Red Deer Wildfire evacuation", "Red Deer Wildfire air quality", "Red Deer Wildfire news", "Red Deer Wildfire update", "Red Deer Wildfire evacuation order",
+    "Red Deer Wildfire evacuation alert", "Red Deer Wildfire evacuation warning", "Red Deer Wildfire evacuation zone", "Red Deer Wildfire evacuation map", "Red Deer Wildfire evacuation route", "Red Deer Wildfire evacuation plan",
+    "Red Deer Wildfire evacuation checklist", "Red Deer Wildfire evacuation kit", "Red Deer Wildfire evacuation center", "Red Deer Wildfire evacuation shelter", "Red Deer Wildfire evacuation hotel",
+    "Calgary Wildfire", "Calgary Wildfire smoke", "Calgary Wildfire evacuation", "Calgary Wildfire air quality", "Calgary Wildfire news", "Calgary Wildfire update", "Calgary Wildfire evacuation order",
+    "Calgary Wildfire evacuation alert", "Calgary Wildfire evacuation warning", "Calgary Wildfire evacuation zone", "Calgary Wildfire evacuation map", "Calgary Wildfire evacuation route", "Calgary Wildfire evacuation plan",
+    "Calgary Wildfire evacuation checklist", "Calgary Wildfire evacuation kit", "Calgary Wildfire evacuation center", "Calgary Wildfire evacuation shelter", "Calgary Wildfire evacuation hotel",
+    "Banff Wildfire", "Banff Wildfire smoke", "Banff Wildfire evacuation", "Banff Wildfire air quality", "Banff Wildfire news", "Banff Wildfire update", "Banff Wildfire evacuation order",
+    "Banff Wildfire evacuation alert", "Banff Wildfire evacuation warning", "Banff Wildfire evacuation zone", "Banff Wildfire evacuation map", "Banff Wildfire evacuation route", "Banff Wildfire evacuation plan",
+    "Banff Wildfire evacuation checklist", "Banff Wildfire evacuation kit", "Banff Wildfire evacuation center", "Banff Wildfire evacuation shelter", "Banff Wildfire evacuation hotel",
+    "British Columbia Wildfire", "British Columbia Wildfire smoke", "British Columbia Wildfire evacuation", "British Columbia Wildfire air quality", "British Columbia Wildfire news", "British Columbia Wildfire update", "British Columbia Wildfire evacuation order",
+    "British Columbia Wildfire evacuation alert", "British Columbia Wildfire evacuation warning", "British Columbia Wildfire evacuation zone", "British Columbia Wildfire evacuation map", "British Columbia Wildfire evacuation route", "British Columbia Wildfire evacuation plan",
+    "British Columbia Wildfire evacuation checklist", "British Columbia Wildfire evacuation kit", "British Columbia Wildfire evacuation center", "British Columbia Wildfire evacuation shelter", "British Columbia Wildfire evacuation hotel"
     ];
   
     try {
